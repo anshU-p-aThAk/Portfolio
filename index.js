5d415593097db5f008c48328d8acd932003c703b
Anshu Pathak <107944211+anshU-p-aThAk@users.noreply.github.com>
2023-07-01 18:41:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 50;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+
+  $('div .icon i').hover(function(){
+    $(this).addClass("fa-bounce", 3000);
+  }, function(){
+    $(this).removeClass("fa-bounce", 3000);
+  });
